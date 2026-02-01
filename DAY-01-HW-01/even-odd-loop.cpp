ef5f1f107496064b60acfd6345b806d15a584eec
ayan khan <ayankhanayan992@gmail.com>
2026-02-02 02:51:29 +0530

    day 01 hw vowels problem

---
+++ DAY-01-HW-01/even-odd-loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n > 1)
+        n -= 2;
+    if (n == 0)
+        cout << "Even" << endl;
+    else
+        cout << "Odd" << endl;
+    return 0;
+}
---
+++ DAY-01-HW-02/vowels-consonants.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    char ch;
+    cout << "Enter an alphabet: ";
+    cin >> ch;
+
+    if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U')
+        cout << "Vowel";
+    else
+        cout << "Consonant";
+
+    return 0;
+}
