207a7f0546d8b89a64ce56ba4c9566ea7aad3fe5
ayan khan <ayankhanayan992@gmail.com>
2026-02-02 02:55:10 +0530

    revision of even odd via loop

--- DAY-01-HW-01/even-odd-loop.cpp
+++ DAY-01-HW-01/even-odd-loop.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main()
 {
+    // This program checks if a number is even or odd using a loop
     int n;
+    cout << "Enter an integer: ";
     cin >> n;
     while (n > 1)
         n -= 2;
