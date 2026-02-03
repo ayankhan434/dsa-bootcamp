2c6ca41eba8ff3b2fbb7f3ae4b84b061c9ac8568
ayan khan <ayankhanayan992@gmail.com>
2026-02-03 20:50:44 +0530

    finding lcm

---
+++ DAY-01-HW-05/lcm.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    int x = a, y = b;
+    while (y != 0)
+    {
+        int temp = y;
+        y = x % y;
+        x = temp;
+    }
+    int hcf = x;
+    int lcm = (a * b) / hcf;
+    cout << "LCM: " << lcm << endl;
+    return 0;
+}
