d668124cbcc87052cf5bfacad4e66fcbbfe88679
ayan khan <ayankhanayan992@gmail.com>
2026-02-08 23:43:06 +0530

    day 02 loop

---
+++ DAY-02/.loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int i = 1;
+    // For loop
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << i;
+    }
+    // While loop
+
+    return 0;
+}
